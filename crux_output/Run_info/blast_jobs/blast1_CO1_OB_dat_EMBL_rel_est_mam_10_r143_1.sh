bddf89bb6b4f0061e7046accb4cbd31049d6289e
lpipes <lenore@tilden.biol.berkeley.edu>
2020-10-16 16:34:51 -0700

    add dir

---
+++ crux_output/Run_info/blast_jobs/blast1_CO1_OB_dat_EMBL_rel_est_mam_10_r143_1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+/bin/bash CRUX_Creating-Reference-libraries-Using-eXisting-tools/crux_db/scripts/sub_blast1.sh -n CO1 -q crux_output/CO1_ecoPCR/cleaned/CO1_OB_dat_EMBL_rel_est_mam_10_r143/blast_ready_1 -o crux_output -k CO1_OB_dat_EMBL_rel_est_mam_10_r143 -l blast_ready_1 -d CRUX_Creating-Reference-libraries-Using-eXisting-tools/crux_db -v 0.00001 -t 10 -i 50 -c 100 -a 10000 -y 1 -z 1
---
+++ crux_output/Run_info/blast_jobs/blast2_CO1_OB_dat_EMBL_rel_est_mam_01_r143_1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+/bin/bash CRUX_Creating-Reference-libraries-Using-eXisting-tools/crux_db/scripts/sub_blast2.sh -n CO1 -q crux_output/CO1_ecoPCR/cleaned/CO1_OB_dat_EMBL_rel_est_mam_01_r143/blast_ready_1 -o crux_output -k CO1_OB_dat_EMBL_rel_est_mam_01_r143 -l blast_ready_1 -d CRUX_Creating-Reference-libraries-Using-eXisting-tools/crux_db -w 0.00001 -j 10 -p 70 -x 70 -a 10000 -y 1 -z 1
